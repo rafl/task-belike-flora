d574645b30738199a263742ed03ad8bb294b0cc3
Florian Ragwitz <rafl@debian.org>
2010-05-07 21:22:37 +0200

    Initial version.

---
+++ lib/Task/BeLike/FLORA.pm
@@ -0,0 +1,125 @@
+use strict;
+use warnings;
+
+package Task::BeLike::FLORA;
+# ABSTRACT: Modules FLORA uses
+
+=pkgroup Applications
+
+=pkg App::Ack
+
+=pkg App::Metabase::Relayd
+
+=pkg App::Nopaste
+
+=pkg App::Software::License
+
+=pkg App::cpanminus
+
+=pkg App::cpanoutdated
+
+=pkg CPAN::Mini
+
+=pkg CPAN::Mini::Devel
+
+=pkg CPAN::Uploader
+
+=pkgroup Optional Dependencies Of Other Modules
+
+=pkg CPAN::Reporter
+
+=pkg PadWalker
+
+=pkg Test::Reporter
+
+=pkg Test::Reporter::Transport::Socket
+
+=pkgroup Development Tools
+
+=pkg Carp::Always
+
+=pkg Carp::Always::Color
+
+=pkg Carp::REPL
+
+=pkg Devel::Cover
+
+=pkg Devel::NYTProf
+
+=pkg Dist::Zilla
+
+=pkg Dist::Zilla::PluginBundle::FLORA
+
+=pkg Gitalist
+
+=pkg Module::Install 0.95
+
+=pkg Module::Install::AuthorRequires
+
+=pkg Module::Install::AuthorTests
+
+=pkg Module::Install::CheckConflicts
+
+=pkg Module::Install::ExtraTests
+
+=pkg Perl::Tidy
+
+=pkg Pod::Coverage::TrustPod
+
+=pkg Test::Pod
+
+=pkg Test::Pod::Coverage
+
+=pkgroup Modules I use a lot
+
+=pkg Catalyst::Model::KiokuDB
+
+=pkg Data::Visitor
+
+=pkg Email::MIME::Kit
+
+=pkg Email::MIME::Kit::Assembler::Markdown
+
+=pkg Email::MIME::Kit::Renderer::TT
+
+=pkg Email::Sender
+
+=pkg Function::Parameters
+
+=pkg JSON
+
+=pkg List::AllUtils
+
+=pkg Method::Signatures::Simple
+
+=pkg Moose
+
+=pkg Moose::Autobox
+
+=pkg MooseX::Declare
+
+=pkg MooseX::LazyRequire
+
+=pkg MooseX::Types::Common
+
+=pkg MooseX::Types::DateTime
+
+=pkg MooseX::Types::Email
+
+=pkg MooseX::Types::LoadableClass
+
+=pkg MooseX::Types::Path::Class
+
+=pkg MooseX::Types::URI
+
+=pkg Task::Catalyst
+
+=pkg Task::KiokuDB
+
+=pkg Task::Plack
+
+=pkg Test::WWW::Mechanize::Catalyst
+
+=cut
+
+1;
